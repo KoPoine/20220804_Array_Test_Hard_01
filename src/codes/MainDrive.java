0b5c7d85a1bfde40d6a92492ab29bab531b76d38
넵플러스 <kmc901206@gmail.com>
2022-08-04 14:45:00 +0900

    1개의 숫자를 뽑는 무한반복문을 총 6번 진행

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -10,20 +10,24 @@ public class MainDrive {
 		
 		int[] myNumbers = new int [6];
 		
-		
-		while (true) {
-			System.out.print("정수 입력 : ");
-			int inputNum = myScanner.nextInt();
-			
-//			받은 정수가 우리가 요구하는 조건을 통과하는지? > 통과한다면 break; while문을 종료
-			boolean isRangeOk = 1 <= inputNum  && inputNum <= 45;
+		for (int i = 0; i < 6; i ++) {
 			
 			
-			if (isRangeOk) {
-				myNumbers[] = inputNum;
-				break;
+			while (true) {
+				System.out.print(i + "번째 숫자 입력 : ");
+				int inputNum = myScanner.nextInt();
+				
+//				받은 정수가 우리가 요구하는 조건을 통과하는지? > 통과한다면 break; while문을 종료
+				boolean isRangeOk = 1 <= inputNum  && inputNum <= 45;
+				
+				
+				if (isRangeOk) {
+					myNumbers[i] = inputNum;
+					break;
+				}
+				
+				
 			}
-			
 			
 		}
 		
