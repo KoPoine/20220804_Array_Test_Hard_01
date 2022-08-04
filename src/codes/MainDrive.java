c6e73f49b5f26694afa63c5d68b5cb2b05ef02dc
넵플러스 <kmc901206@gmail.com>
2022-08-04 14:42:54 +0900

    제한사항1. 뽑은 숫자(inputNum)는 1이상 45이하여야 한다.

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -10,11 +10,39 @@ public class MainDrive {
 		
 		int[] myNumbers = new int [6];
 		
+		
 		while (true) {
 			System.out.print("정수 입력 : ");
 			int inputNum = myScanner.nextInt();
+			
+//			받은 정수가 우리가 요구하는 조건을 통과하는지? > 통과한다면 break; while문을 종료
+			boolean isRangeOk = 1 <= inputNum  && inputNum <= 45;
+			
+			
+			if (isRangeOk) {
+				myNumbers[] = inputNum;
+				break;
+			}
+			
+			
 		}
 		
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
