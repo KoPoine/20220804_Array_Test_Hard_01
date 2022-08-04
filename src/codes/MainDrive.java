cf6192c0eecbbb391d1cbd16d0e329a5b7dfe5f0
넵플러스 <kmc901206@gmail.com>
2022-08-04 14:54:04 +0900

    내 숫자 6개 뽑는 로직 작성 완료

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -14,14 +14,25 @@ public class MainDrive {
 			
 			
 			while (true) {
-				System.out.print(i + "번째 숫자 입력 : ");
+				System.out.print((i + 1) + "번째 숫자 입력 : ");
 				int inputNum = myScanner.nextInt();
 				
 //				받은 정수가 우리가 요구하는 조건을 통과하는지? > 통과한다면 break; while문을 종료
 				boolean isRangeOk = 1 <= inputNum  && inputNum <= 45;
 				
+				boolean isRepeat = false;
 				
-				if (isRangeOk) {
+//				중복을 확인하는 for-each > isRepeat 변수 값을 변경
+//				 => 값을 대입하는게 아닌, 조회해서 우리가 뽑은 숫자(inputNum)과의 중복 여부
+				for (int num : myNumbers) {
+					
+					if (num == inputNum) {
+						isRepeat = true;
+						break;
+					}					
+				}	
+				
+				if (isRangeOk && !isRepeat) {
 					myNumbers[i] = inputNum;
 					break;
 				}
