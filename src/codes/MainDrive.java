1ffaae9cf3406737e90b2e24b30a2dead515714c
넵플러스 <kmc901206@gmail.com>
2022-08-04 14:37:15 +0900

    정수를 무한하게 입력받을 수 있도록 Scanner 설치 / 무한 반복문 작성

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,10 +1,19 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
 
+		Scanner myScanner = new Scanner(System.in);
 		
+		int[] myNumbers = new int [6];
+		
+		while (true) {
+			System.out.print("정수 입력 : ");
+			int inputNum = myScanner.nextInt();
+		}
 		
 	}
 	
